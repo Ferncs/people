b1d0e114582574e2c663091c917c1f5a613408d2
Fernando Sato <ferncs89@gmail.com>
2018-09-16 21:41:21 -0300

    App.js

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
 
     renderList(){
-        const names = [
-        'Goku',
-        'Kuririn',
-        'Vegeta',
-        'Gohan' 
-      ];
-       const textElements = names.map(name => {
-          return <Text key ={name}>{name}</Text>
+       /*const textElements = names.map(name => {
+        return <Text key ={name}>{name}</Text>
         }
-      );
+      );*/
+      /Promises/
+      axios
+      .get('https://randomuser.me/api/?nat=br&results=5')
+      .then(response =>{
+          const { results } = response.data;
+          const names  = results.map(people => people.name.first);
+          console.log(names);
+      })
 
-      return textElements;
+
+
+
+      //return textElements;
     }
-
 
   render() {
     return (
